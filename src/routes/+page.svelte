9d5aa3e85785c35f2fb0e9f4221395a2a21fc936
Alice Feng <feng.a.g@gmail.com>
2024-08-18 17:30:04 -0400

    Build out intro section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,49 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+    <section class="intro">
+        <div>Hello world, this is</div>
+        <h1>Alice Feng.</h1>
+        <p>I’m a data communicator passionate about visualizing data in equitable, inclusive, and accessible ways.</p>
+        <p>I’ve analyzed data and created visualizations at places such as Axios, the United Nations Development Programme, and the Urban Institute and have worked with organizations such as Data4Change, The Pudding, and DataKind. I’m also one of the authors of the <a href="https://www.urban.org/research/publication/do-no-harm-guide-applying-equity-awareness-data-visualization" target="_blank">Do No Harm Guide</a>.</p> 
+        <nav>
+            Check out: 
+            <a href="#">My work</a>
+            <a href="#">My writing</a>
+            <a href="#">My teaching</a>
+        </nav>
+    </section>
+</main>
+
+<style lang="scss">
+    main {
+        max-width: 1000px;
+        width: 100%;
+        margin: 0 auto;
+        /* border: 1px solid rebeccapurple; */
+    }
+
+    .intro {
+        div {
+            font-size: 3.125rem;
+            margin-top: 4em;
+        }
+
+        h1 {
+            font-size: 8.75rem;
+            line-height: 1.2;
+            font-weight: 700;
+            margin: 0;
+        }
+
+        p{
+            font-size: 2rem;
+            margin: 1em 0;
+        }
+
+        nav {
+            font-size: 2rem;
+            font-weight: 700;
+            display: flex;
+            gap: 2.5em;
+        }
+    }
+</style>
