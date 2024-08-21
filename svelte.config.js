21e1379db92e304d76779ceb820b919ef8573009
Alice Feng <feng.a.g@gmail.com>
2024-08-21 14:17:19 -0400

    Change app directory output name to remove underscore

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,8 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			fallback: '404.html'
-		})
+		}),
+		appDir: 'app'
 	},
 	preprocess: [vitePreprocess()]
 };
