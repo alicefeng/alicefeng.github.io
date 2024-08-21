32fba01de4348dfcc06bca95328c52600975030a
Alice Feng <feng.a.g@gmail.com>
2024-08-21 14:25:55 -0400

    Add gh-pages package to deploy site to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { defineConfig } from 'vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 
 export default defineConfig({
+	base: "/alicefeng.github.io/",
 	plugins: [enhancedImages(), sveltekit()]
 });
